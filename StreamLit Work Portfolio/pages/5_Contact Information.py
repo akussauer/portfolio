da607ee18a4c1f09dcfa4eb70971cfc6716456a4
Alex Kussauer <akussauer@gmail.com>
2025-04-16 15:59:17 +0100

    Add files via upload

---
+++ StreamLit Work Portfolio/pages/5_Contact Information.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Alex Kussauer - Contact Info",
+    page_icon=":wave:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# --- Contact Information ---
+st.header("📫 Contact Information")
+st.divider()
+st.write("If you'd like more information about me, feel free to reach out!")
+st.write("Email: akussauer@gmail.com")
+st.write("LinkedIn: https://www.linkedin.com/in/alex-kussauer-771b782ab/")
